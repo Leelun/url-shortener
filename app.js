a9cf510f0888d4ae215ae5e62e1125b6af19207e
Leelun <louie011106@gmail.com>
2023-04-05 18:03:12 +0800

    feat: add bootstrap & modify index.hbs

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const exphbs = require('express-handlebars')
 const port = 3000
 const app = express()
 
+
+
+
 if (process.env.NODE_ENV !== 'production') { //僅在非正式環境時, 使用 dotenv
   require('dotenv').config()
 }
+
+
+
+
 mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', () => {
