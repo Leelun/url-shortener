67fcc252861227262c4078d0d37bc80ab32f8456
Leelun <louie011106@gmail.com>
2023-04-05 17:29:33 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const port = 3000
 const app = express()
@@ -16,9 +17,25 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
-  res.send('一切順利')
+  res.render('index')
 })
+
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`app is listen on http://localhost:${port}`)
