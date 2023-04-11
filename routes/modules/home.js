c31403556a2d846ab888f617e5aecdead4f4f285
Leelun <louie011106@gmail.com>
2023-04-11 22:31:53 +0800

    add comment on home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -14,22 +14,20 @@ router.post('/toShorten', (req, res) => {
   const urlInput = req.body.urlInput
   let path = getRandomUrlOutput()
   const urlOutput = `localhost:${port}/ShortenDone/${path}`
-  function check() {
+  function check() { //使用遞迴函式檢查是否有重複的網址
     UrlShortener.exists({ urlOutput: `localhost:${port}/ShortenDone/${path}` })
       .then((result) => {
-        if (result) {
+        if (result) { //如果有的話則重新產生一組path
           path = getRandomUrlOutput()
           check()
-        } else {
+        } else { //沒有的話check是否輸入相同的網址
           UrlShortener.findOne({ urlInput })
-
-            .then((data) => {
-              if (data) {
+            .then((data) => { 
+              if (data) { //是的話則把已經縮短過的網址render出來
                 res.render('output', { urlOutput: data.urlOutput }) 
-              } else {
+              } else {//否的話則去建立一組新的urlInput和urlOutput
                 UrlShortener.create({ urlInput, urlOutput })
-
-                  .then(() => {
+                  .then(() => { 
                     res.render('output', { urlOutput })
                   })
                   .catch(error => console.log(error))
@@ -42,7 +40,7 @@ router.post('/toShorten', (req, res) => {
   check();
 })
 
-router.get('/ShortenDone/:shorten', (req, res) => {
+router.get('/ShortenDone/:shorten', (req, res) => { //輸入短網址導向原本的網址
   const shorten = req.params.shorten
   UrlShortener.findOne({ urlOutput: `localhost:${port}/ShortenDone/${shorten}` })
     .lean()
